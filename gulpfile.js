3c42fec9f153a39e2a09711eeeb346d6ff717916
sergiyKob <sergiykob799@gmail.com>
2022-05-18 16:32:21 +0300

    test new

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-// const scss         = require('gulp-sass')(require('sass'));
+
 const scss         = require('gulp-sass')(require('sass'));
 const concat       = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
@@ -9,6 +9,9 @@ const imagemin     = require('gulp-imagemin');
 const del          = require('del');
 const { cleanup }  = require('browser-sync');
 const browserSync  = require('browser-sync').create();
+const svgSprite    = require('gulp-svg-sprite');
+const cheerio      = require('gulp-cheerio');
+const replace      = require('gulp-replace');
 
 
 function browsersync() {
@@ -37,6 +40,7 @@ function styles() {
 function scripts() {
   return src ([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -70,6 +74,31 @@ function build() {
   .pipe(dest('dist'))
 }
 
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(cheerio({
+      run: ($) => {
+        $("[fill]").removeAttr("fill");
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: { xmlMode: true },
+    })
+    )
+    .pipe(replace('&gt;', '>')) // боремся с заменой символа 
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
+}
+
+
 function cleanDist() {
   return del('dist')
 }
@@ -79,6 +108,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 exports.styles = styles;
@@ -88,8 +118,10 @@ exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
+exports.svgSprites = svgSprites;
 
 
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
 
