8b5e669a071cbf5f7b0a7ec0c4015e07aa566027
sergiyKob <sergiykob799@gmail.com>
2022-08-20 15:49:11 +0300

    added section app

--- app/js/main.js
+++ app/js/main.js
@@ -8,9 +8,11 @@ $(function () {
   });
 
 
-  $('.reviews__inner').slick({
+  $('.reviews__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><svg class="slick-btn__arrow"><use xlink: href = "images/sprite.svg#left-slider"></use></svg></button>',
+
+    nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="slick-btn__arrow"><use xlink: href = "images/sprite.svg#right-slider"></use></svg ></button>',
     dots: true,
-    arrows: true,
     fade: true,
   });
 
